30155dedef92045246daac8e44452355ff0d073c
Steve Calabro <steve.a.calabro@gmail.com>
2016-02-23 00:20:06 -0500

    Base for project

---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name craftMateApp.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the craftMateApp
+ */
+angular.module('craftMateApp')
+.controller('HomeCtrl', function () {
+});
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name craftMateApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the craftMateApp
+ */
+angular.module('craftMateApp')
+.controller('MainCtrl', function () {
+
+});
