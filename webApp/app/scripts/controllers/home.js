563cfd275ed7d3459b94e7f6851f9636794b5bf3
Steve Calabro <steve.a.calabro@gmail.com>
2016-03-01 23:00:44 -0500

    Fully working history

--- webApp/app/scripts/controllers/home.js
+++ webApp/app/scripts/controllers/home.js
@@ -8,11 +8,12 @@
  * Controller of the craftMateApp
  */
 angular.module('craftMateApp')
-.controller('HomeCtrl', function ($scope, $http, $filter) {
+.controller('HomeCtrl', function ($scope, $http, $filter, $timeout, $rootScope) {
 	$scope.bookmarks = [];
 	$scope.history = []
 	$scope.recipes = [];
 	$scope.searchText;
+	$scope.page = 0;
 
 	$scope.recipe = {};
 
@@ -31,9 +32,11 @@ angular.module('craftMateApp')
 	$scope.stackSize;
 	$scope.recipeName;
 
-	for(var i = 0; i < 3; i ++) {
+	for(var i = 0; i < 3; i++) {
 		for(var j = 0; j < 3; j++) {
-			$scope.inputImages[i][j] = 'display: none;';
+			if($scope.inputImages[i][j] == "" || !$scope.inputImages[i][j]) {
+				$scope.inputImages[i][j] = 'display: none;';
+			}
 		}
 	}
 
@@ -46,8 +49,27 @@ angular.module('craftMateApp')
 		console.log("Bookmark Clicked" + index);
 	}
 
+	$scope.pageChangeHandler = function (num) {
+        $scope.page = num - 1;
+    };
+
 	$scope.goToHistory = function(index) {
-		console.log("History Clicked" + index);
+		index = index + ($scope.page * 5);
+		//console.log('Go history' + index);
+		var recipe = $scope.history[index].recipe;
+		$scope.recipe = recipe;
+
+		$scope.recipeName = recipe.name;
+		$scope.stackSize = recipe.stackSize;
+		$scope.recipeMap = recipe.recipeMap;
+
+		$scope.inputImages = recipe.inputImages;
+		
+		if(recipe.outputImage.includes("background-position")) {
+			$scope.outputSprite = recipe.outputImage;
+		} else {
+			$scope.outputImage = recipe.outputImage;
+		}
 	}
 
     $scope.querySearch = function(query) {
@@ -59,26 +81,42 @@ angular.module('craftMateApp')
     }
 
     $scope.addToHistory = function() {
-    	if($scope.recipe.name && $scope.recipe.name != "") {
-    		$scope.history.unshift({id: $scope.history.length -1, recipe: $scope.recipe});
-    	}
-    	
-    	$scope.searchText = '';
- 
-		$scope.outputImage = null;
-		$scope.outputSprite = 'display:none;';
-		$scope.stackSize = null;
-		$scope.recipeName = null;
+    	if($scope.recipe.name) {
+	    	if($scope.recipe.name && $scope.recipe.name != "") {
+	    		if($scope.history.length > 0) {
+	    			if($scope.history[0].recipe.name != $scope.recipe.name) {
+	    				$scope.history.unshift({id: $scope.history.length -1, recipe: $scope.recipe});
+	    			}
+	    		} else {
+	    			$scope.history.unshift({id: $scope.history.length -1, recipe: $scope.recipe});
+	    		}
+	    	}
+	    	
+			$scope.outputSprite = "display: none;";
+	    	$scope.outputImage = "";
+	    	$scope.searchText = '';
+			$scope.stackSize = null;
+			$scope.recipeName = null;
 
-		for(var i = 0; i < 3; i ++) {
-			for(var j = 0; j < 3; j++) {
-				$scope.inputImages[i][j] = 'display: none;';
+			$scope.inputImages = new Array(3);
+			for(var i = 0; i < 3; i++) {
+				$scope.inputImages[i] = new Array(3);
 			}
-		}
 
-		$scope.recipeMap = new Array(3);
-		for(var i = 0; i < 3; i++) {
-			$scope.recipeMap[i] = new Array(3);
+			$scope.recipeMap = new Array(3);
+			for(var i = 0; i < 3; i++) {
+				$scope.recipeMap[i] = new Array(3);
+			}
+
+			$timeout(function() {
+			    $scope.outputSprite = "display: none;";
+			    $scope.stackSize = null;
+
+			    $scope.inputImages = new Array(3);
+				for(var i = 0; i < 3; i++) {
+					$scope.inputImages[i] = new Array(3);
+				}
+			}, 10);
 		}
     }
 
@@ -92,14 +130,6 @@ angular.module('craftMateApp')
     		$scope.recipeMap = recipe.recipeMap;
     		$scope.inputImages = recipe.inputImages;
 
-    		for(var i = 0; i < 3; i++) {
-    			for(var j = 0; j < 3; j++) {
-    				if($scope.inputImages[i][j] == "") {
-    					$scope.inputImages[i][j] = 'display: none;';
-    				}
-    			}
-    		}
-
     		if(recipe.outputImage.includes("background-position")) {
     			$scope.outputSprite = recipe.outputImage;
     		} else {
@@ -107,4 +137,14 @@ angular.module('craftMateApp')
     		}
     	}
     }
+
+    $scope.$watch('inputImages', function() {
+    	for(var i = 0; i < 3; i++) {
+			for(var j = 0; j < 3; j++) {
+				if($scope.inputImages[i][j] == "" || !$scope.inputImages[i][j]) {
+					$scope.inputImages[i][j] = 'display: none;';
+				}
+			}
+		}
+    });
 });
