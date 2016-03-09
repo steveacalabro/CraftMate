c1e2f9a167874db8f27411fa20dcb94625cd591f
Stephen Calabro <steve.a.calabro@gmail.com>
2016-03-09 17:59:34 -0500

    Working elctron file

--- main.js
+++ main.js
@@ -1,43 +1,31 @@
 'use strict';
+const app = require('app');
+const BrowserWindow = require('browser-window');
 
-const electron = require('electron');
-const app = electron.app; // Module to control application life.
-const BrowserWindow = electron.BrowserWindow; // Module to create native browser window.
+// report crashes to the Electron project
+require('crash-reporter').start();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
+// prevent window being GC'd
+let mainWindow = null;
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', function () {
-    // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 800,
         height: 700,
         icon: 'icon.png'
     });
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/webApp/app/index.html');
+    mainWindow.loadUrl(`file://${__dirname}/webApp/app/index.html`);
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-
-    // Emitted when the window is closed.
     mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
+        // deref the window
+        // for multiple windows store them in an array
         mainWindow = null;
     });
 });
